2e08ff7ef291f0c53e8b33e462a445ca90438084
Allen Thomas <at2224@njit.edu>
2025-02-09 20:50:57 -0500

    adding comments

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+// getting elements from html
 const submitButton = document.getElementById('submit-button');
 const list = document.getElementById('list');
 const note = document.getElementById('note');
@@ -6,6 +7,7 @@ const note = document.getElementById('note');
 submitButton.onclick = submiteNote;
 
 function submiteNote() {
+    // when I click submit, uses what user types in input field to create a note with a delete button in the list container below
     const newNote = document.createElement('li');
     const newDelete = document.createElement('button');
     newNote.textContent = note.value;
