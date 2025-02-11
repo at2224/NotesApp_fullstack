5e481cb099398e38d5fe3763961ada47b6d887c6
Allen Thomas <at2224@njit.edu>
2025-02-10 23:51:51 -0500

    fin delete button func. need to save to local now

--- public/script.js
+++ public/script.js
@@ -6,13 +6,33 @@ const note = document.getElementById('note');
 // initialize buttons
 submitButton.onclick = submiteNote;
 
+// add event listener for delete button since I have multiple and they are added dynamically.
+// queryselectorall returns a nodelist so cant do .onclick on the list. have to get each element from the list and onclick on each ind. button
+// but queryselector and then a for each would only work on hardcoded delete buttons, not dynamically added ones
+// document.addEventListener('click',) for dynamic buttons
+document.addEventListener('click', (event)=> {
+    if (event.target.classList.contains("delete-button")) {
+        deleteNote(event); // parenthsis to call the function rather than pass a reference to it
+    }
+})
+
 function submiteNote() {
     // when I click submit, uses what user types in input field to create a note with a delete button in the list container below
     const newNote = document.createElement('li');
     const newDelete = document.createElement('button');
+    newDelete.classList.add('delete-button');
     newNote.textContent = note.value;
     newDelete.textContent = "delete";
-    console.log(newNote);
+    newNote.appendChild(newDelete);
     list.appendChild(newNote);
-    list.appendChild(newDelete);
+    //list.appendChild(newDelete);
+}
+
+function deleteNote(event) {
+    // trick here is to append button as a child to li then use .closest to remove the li
+    // doesnt delete anything if delete isnt appended to li tho for some reason 
+    const liToDelete = event.target.closest("li");
+    if (liToDelete) {
+        list.removeChild(liToDelete);
+    }
 }
