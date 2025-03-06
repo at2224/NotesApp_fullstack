06c079462339cd983a9cc2ae9bae490e844463a2
Allen Thomas <at2224@njit.edu>
2025-03-06 12:39:12 -0500

    serve html on server successfully

--- src/server.js
+++ src/server.js
@@ -1,14 +1,28 @@
 import express from 'express'; // install express
 import notes from './routes/notes.js';
+import path, {dirname} from 'path';
+import { fileURLToPath } from 'url';
 
 // set up app
 const app = express();
 // specify port
-const PORT = process.env.PORT || 5432;
+const PORT = 5000;
 
 // serve html
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '../public/index.html');
+    try {
+        res.sendFile(path.join(__dirname, '../public', 'index.html'));
+        console.log("hi");
+    }
+    catch (err) {
+        console.error(err.message);
+    }
 })
 
 app.use('/api', notes);
