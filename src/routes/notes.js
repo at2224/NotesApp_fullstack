d280e72232d88fa41d1a6da0256af85b9a9593f6
at2224 <at2224@njit.edu>
2025-03-05 23:29:15 -0500

    get req

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -4,10 +4,22 @@ import pool from '../db.js';
 const router = express.Router();
 
 // save note to db
-router.post('/save', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const {note_text} = req.body;
-        await pool.query("INSERT INTO notesDB VALUES ($1)", [note_text]);
+        await pool.query("INSERT INTO notesDB VALUES $1", [note_text]);
+    }
+    catch (err) {
+        console.err(err.message);
+    }
+});
+
+// get note on reload
+router.post('/', async (req, res) => {
+    try {
+        const {note_id} = req.body;
+        const result = await pool.query("SELECT * FROM notesDB where id = $1", [note_id]);
+        res.json(result.rows);
     }
     catch (err) {
         console.err(err.message);
